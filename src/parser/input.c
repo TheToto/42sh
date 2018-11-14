7ecaff653d03ffb756d8d67bea263e20f3f7069f
Thomas Lupin <thomas.lupin@epita.fr>
2018-11-14 20:28:28 +0100

    [PARSER] Add more parser funcs

--- src/parser/input.c
+++ src/parser/input.c
@@ -1,10 +1,13 @@
+#include <err.h>
 #include "parser.h"
 #include "ast.h"
 
 #define NEXT_TOK(Tok) (*Tok = (*Tok)->next)
 
-#define TOK_TYPE(Tok) (*(Tok)->type)
-#define NEXT_TOK_TYPE(Tok) (*(Tok)->next->type)
+#define TOK_TYPE(Tok) ((*Tok)->type)
+#define TOK_STR(Tok) ((*Tok)->str)
+#define NEXT_TOK_TYPE(Tok) ((*Tok)->next->type)
+#define NEXT_TOK_STR(Tok) ((*Tok)->next->str)
 
 void remove_new_line(struct token_list **tok)
 {
@@ -14,14 +17,14 @@ void remove_new_line(struct token_list **tok)
 
 struct ast_node *rule_input(struct token_list **tok)
 {
-    if (TOK_TYPE(tok) == NEWLINE || TOK_TYPE(tok) == EOF)
+    if (TOK_TYPE(tok) == NEWLINE || TOK_TYPE(tok) == END_OF_FILE)
     {
         // DO NOTHING (Not an error)
         return NULL;
     }
     NEXT_TOK(tok);
     struct ast_node *res = rule_list(tok);
-    if (TOK_TYPE(tok) == NEWLINE || TOK_TYPE(tok) == EOF)
+    if (TOK_TYPE(tok) == NEWLINE || TOK_TYPE(tok) == END_OF_FILE)
     {
         return res;
     }
@@ -32,14 +35,14 @@ struct ast_node *rule_input(struct token_list **tok)
 struct ast_node *rule_list(struct token_list **tok)
 {
     struct ast_node *left_andor = rule_andor(tok);
-    if (!res_andor)
+    if (!left_andor)
         // ERROR
         return NULL;
     if (TOK_TYPE(tok) == SEMICOLON || TOK_TYPE(tok) == AMPERSAND)
     {
         enum token_type save = TOK_TYPE(tok);
         NEXT_TOK(tok);
-        if (TOK_TYPE(tok) == NEWLINE || TOK_TYPE(tok) == EOF)
+        if (TOK_TYPE(tok) == NEWLINE || TOK_TYPE(tok) == END_OF_FILE)
         {
             if(save == AMPERSAND)
                 return create_ast_node_ampersand(left_andor, NULL);
@@ -109,5 +112,102 @@ struct ast_node *rule_pipe(struct token_list **tok)
 
 struct ast_node *rule_command(struct token_list **tok)
 {
+    if (TOK_TYPE(tok) == BRACKET_ON
+            || TOK_TYPE(tok) == PARENTHESIS_ON
+            || TOK_TYPE(tok) == FOR
+            || TOK_TYPE(tok) == WHILE
+            || TOK_TYPE(tok) == UNTIL
+            || TOK_TYPE(tok) == CASE
+            || TOK_TYPE(tok) == IF)
+    {
+        //NEXT_TOK(tok);
+        return rule_shell_command(tok);
+    }
+    if ((TOK_TYPE(tok) == WORD && !strcmp(TOK_STR(tok), "function"))
+            || (TOK_TYPE(tok) == WORD && NEXT_TOK_TYPE(tok) == PARENTHESIS_ON))
+    {
+        return rule_funcdec(tok)
+    }
+    // HANDLE ERRORS HERE PLEASE
+    return rule_simple_command(tok);
+}
+
+struct ast_node *rule_shell_command(struct token_list **tok)
+{
+    if (TOK_TYPE(tok) == IF)
+        return rule_if(tok);
+    if (TOK_TYPE(tok) == FOR)
+        return rule_for(tok);
+    if (TOK_TYPE (tok) == WHILE)
+        return rule_while(tok);
+    if (TOK_TYPE(tok) == UNTIL)
+        return rule_until(tok);
+    if (TOK_TYPE(tok) == CASE)
+        return rule_case(tok);
+    if (TOK_TYPE(tok) == BRACKET_ON)
+        return rule_compound_list(tok);
+    if (TOK_TYPE(tok) == PARENTHESIS_ON)
+    {
+        NEXT_TOK(tok);
+        struct ast_node *res = rule_compound_list(tok);
+        if (TOK_TYPE(tok) != BRACKET_OFF && TOK_TYPE(tok) != PARENTHESIS_OFF)
+            // ERROR
+            errx(1, "Handle errors better please");
+        NEXT_TOK(tok);
+        return res;
+    }
+    errx(1, "Handle errors better please");
+}
+
+
+struct ast_node *rule_if(struct token_list **tok)
+{
     return NULL;
 }
+
+struct ast_node *rule_while(struct token_list **tok)
+{
+    return NULL;
+}
+
+struct ast_node *rule_until(struct token_list **tok)
+{
+    return NULL;
+}
+
+struct ast_node *rule_case(struct token_list **tok)
+{
+    return NULL;
+}
+
+struct ast_node *rule_compound_list(struct token_list **tok)
+{
+    return NULL;
+}
+
+struct ast_node *rule_funcdec(struct token_list **tok)
+{
+    errx(1,"Func dec missing");
+    return NULL;
+}
+
+struct ast_node *rule_simple_command(struct token_list **tok)
+{
+    return NULL;
+}
+
+struct ast_node *rule_compound_prefix(struct token_list **tok)
+{
+    return NULL;
+}
+
+struct ast_node *rule_compound_element(struct token_list **tok)
+{
+    return NULL;
+}
+
+struct ast_node *rule_compound_redirection(struct token_list **tok)
+{
+    return NULL;
+}
+
