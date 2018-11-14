4acd06a7d6d3dd66bcb5ad8ccfa9c00c06fc311b
Onolwe <arthur.busuttil@neuf.fr>
2018-11-14 15:47:00 +0100

    [LEXER][UPDATE] Update list of token and lexer consiquently

--- src/include/parser.h
+++ src/include/parser.h
@@ -4,20 +4,50 @@
 
 enum token_type
 {
-    IF,
-    THEN,
-    ELSE,
-    FI,
-    WHILE,
-    FOR,
-    IN,
-    DO,
-    DONE,
-    SEMICOLON,
-    LOGICAL_AND,
-    LOGICAL_OR,
-    WORD,
-// ...
+    //REDIRECTION
+    LESS,               //<
+    GREAT,              //>
+    DLESS,              //<<
+    DGREAT,             //>>
+    LESSAND,            //<&
+    GREATAND,           //>&
+    LESSGREAT,          //<>
+    DLESSDASH,          //<<-
+    CLOBBER,            //>|
+    PIPE,               //|
+    //CONDITION
+    IF,                 //if
+    THEN,               //then
+    ELIF,               //elif
+    ELSE,               //esle
+    FI,                 //fi
+    //CASE
+    CASE,               //case
+    ESAC,               //esac
+    DSEMICOLON,         //;;
+    //LOOP
+    WHILE,              //while
+    UNTIL,              //until
+    FOR,                //for
+    DO,                 //do
+    DONE,               //done
+    SEMICOLON,          //;
+    //SPECIAL
+    IN,                 //in
+    BRACKET_ON,         //{
+    BRACKET_OFF,        //}
+    PARENTHESIS_ON,      //(
+    PARENTHESIS_OFF,     //)
+    //BIN OPERATOR
+    LOGICAL_AND,        //&&
+    LOGICAL_OR,         //||
+    ESPERLUETTE,        //&
+    //DEFAULT
+    IO_NUMBER,          //[1-9]+
+    NEWLINE,            //\n
+    ASSIGNMENT_WORD,    //*=$
+    NAME,               //[_a-zA-Z][_1-9a-zA-Z]*
+    WORD,               //le reste
 };
 
 struct token_list
--- src/lexer.c
+++ src/lexer.c
@@ -1,20 +1,66 @@
 #include <stdlib.h>
 #include <string.h>
+#include <fnmatch.h>
 
 #include "parser.h"
 
-enum token_type get_token_type(char *val)
+enum token_type redirection_token(char *val)
+{
+    if (strcmp(val, "<"))
+        return LESS;
+    if (strcmp(val, ">"))
+        return GREAT;
+    if (strcmp(val, "<<"))
+        return DLESS;
+    if (strcmp(val, ">>"))
+        return DGREAT;
+    if (strcmp(val, "<&"))
+        return LESSAND;
+    if (strcmp(val, ">&"))
+        return GREATAND;
+    if (strcmp(val, "<>"))
+        return LESSGREAT;
+    if (strcmp(val, "<<-"))
+        return DLESSDASH;
+    if (strcmp(val, ">|"))
+        return CLOBBER;
+    if (strcmp(val, "|"))
+        return PIPE;
+    return WORD;
+}
+
+enum token_type condition_token(char *val)
 {
     if (!strcmp(val, "if"))
         return IF;
     if (!strcmp(val, "then"))
         return THEN;
+    if (!strcmp(val, "elif"))
+        return ELIF;
     if (!strcmp(val, "else"))
         return ELSE;
     if (!strcmp(val, "fi"))
         return FI;
+    return WORD;
+}
+
+enum token_type case_token(char *val)
+{
+    if (!strcmp(val, "case"))
+        return CASE;
+    if (!strcmp(val, "esac"))
+        return ESAC;
+    if (!strcmp(val, ";;"))
+        return DSEMICOLON;
+    return WORD;
+}
+
+enum token_type loop_token(char *val)
+{
     if (!strcmp(val, "while"))
         return WHILE;
+    if (!strcmp(val, "until"))
+        return UNTIL;
     if (!strcmp(val, "for"))
         return FOR;
     if (!strcmp(val, "in"))
@@ -25,11 +71,64 @@ enum token_type get_token_type(char *val)
         return DONE;
     if (!strcmp(val, ";"))
         return SEMICOLON;
+    return WORD;
+}
+
+enum token_type special_token(char *val)
+{
+    if (!strcmp(val, "in"))
+        return IN;
+    if (!strcmp(val, "{"))
+        return BRACKET_ON;
+    if (!strcmp(val, "}"))
+        return BRACKET_OFF;
+    if (!strcmp(val, "("))
+        return PARENTHESIS_ON;
+    if (!strcmp(val, ")"))
+        return PARENTHESIS_OFF;
+    return WORD;
+}
+
+enum token_type bin_op_token(char *val)
+{
     if (!strcmp(val, "&&"))
         return LOGICAL_AND;
     if (!strcmp(val, "||"))
         return LOGICAL_OR;
+    if (!strcmp(val, "&"))
+        return ESPERLUETTE;
     return WORD;
+}
+
+enum token_type default_token(char *val)
+{
+    if (!fnmatch(val, "[1-9]+", FNM_NOESCAPE))
+        return IO_NUMBER;
+    if (!strcmp(val, "\n"))
+        return NEWLINE;
+    if (!fnmatch(val, "*=$", FNM_NOESCAPE))
+        return ASSIGNMENT_WORD;
+    if (!fnmatch(val, "[_a-zA-Z][_1-9a-zA-Z]*", FNM_NOESCAPE))
+        return NAME;
+    return WORD;
+}
+
+enum token_type get_token_type(char *val)
+{
+    int res = redirection_token(val);
+    if (res == WORD)
+        res = condition_token(val);
+    if (res == WORD)
+        res = case_token(val);
+    if (res == WORD)
+        res = loop_token(val);
+    if (res == WORD)
+        res = special_token(val);
+    if (res == WORD)
+        res = bin_op_token(val);
+    if (res == WORD)
+        res = default_token(val);
+    return res;
 }
 
 void lexer_destroy(struct lexer *l)
