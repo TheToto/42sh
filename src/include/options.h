87db75feff6539008e6590198a5c02f72bd3ef85
Sabrina Meng <sabrina.meng@epita.fr>
2018-11-15 11:29:35 +0100

    [OPTIONS] Added doc for Doxygen in options.c and options.h

--- src/include/options.h
+++ src/include/options.h
@@ -1,13 +1,23 @@
 #pragma once
 
+/**
+ *\brief enum of the different options
+ *\details List all the options handled by the 42sh
+ */
 enum option
 {
-    NONE = 0,
-    SHOPT,
-    CMD,
-    NORC,
-    AST,
-    VERSION,
+    NONE = 0,///<Unknown or no options
+    SHOPT,///<[-+]O        Enables shopt variables
+    CMD,///<-c <command>   Executes the command
+    NORC,///<--norc        Deactivates ressource reader
+    AST,///<--ast-print    Prints the AST
+    VERSION,///<--version  Prints the current version on the standard output
 };
 
+/**
+ *\fn options
+ *\brief Do actions according to each options
+ *\param char *argv[]   The command line to parse
+ *\return The return value depends on the options
+ */
 void options(char *argv[]);
--- src/options.c
+++ src/options.c
@@ -1,3 +1,13 @@
+/**
+ *\file options.c
+ *\author sabrina.meng
+ *\version 0.3
+ *\date 15-11-2018
+ *\brief Options parsing
+ *\detail Parse the options with the format
+ *[GNU long option] [option] script-file
+ */
+
 #include <string.h>
 #include <stdio.h>
 #include <err.h>
@@ -5,95 +15,94 @@
 
 #include "options.h"
 
-// Get the option type accordig to the enum
-static enum option get_option(char *opt, size_t section)
+/**
+ *\fn get_section
+ *\brief Check if it is long option, option or script-file
+ *\param char *arg  The argument to check
+ *\return Return a size_t representing the section
+ */
+static size_t get_section(char *arg)
 {
-    if (!section)
+    if (arg[0] == '-')
     {
-        if (!strcmp(opt, "--norc"))
-            return NORC;
-        if (!strcmp(opt, "--ast-print"))
-            return AST;
-        if (!strcmp(opt, "--version"))
-            return VERSION;
-        if (!strcmp(opt, "-c"))
-            return CMD;
-        if (!strcmp(opt, "-O") || !strcmp(opt, "+O"))
-            return SHOPT;
+        if (arg[1] && arg[1] == '-')
+            return 0;
+        return 1;
     }
+    return 2;
+}
+
+/**
+ *\fn get_option
+ *\brief Get the option type according to the enum of the header
+ *\param char *opt  The option to check
+ *\return Return an enum value acoording to the option
+ */
+static enum option get_option(char *opt)
+{
+    if (!strcmp(opt, "--norc"))
+        return NORC;
+    if (!strcmp(opt, "--ast-print"))
+        return AST;
+    if (!strcmp(opt, "--version"))
+        return VERSION;
+    if (!strcmp(opt, "-c"))
+        return CMD;
+    if (!strcmp(opt, "-O") || !strcmp(opt, "+O"))
+        return SHOPT;
     return NONE;
 }
 
-// Check if all options are correct
-static int check_options(char *argv[])
+/**
+ *\fn options
+ *\brief Do actions according to each options
+ *\param char *argv[]   The command line to parse
+ *\return The return value depends on the options
+ */
+void options(char *argv[])
 {
     enum option opt = NONE;
     size_t section = 0;
 
     for (size_t i = 1; argv[i]; i++)
     {
-        section = argv[i][0] == '-' ? 0 : 1;
-        opt = get_option(argv[i], section);
+        size_t sect = get_section(argv[i]);
+        section = section > sect ? section : sect;
+        opt = get_option(argv[i]);
         switch (opt)
         {
             case CMD:
-                if (!(argv[++i] && argv[i][0] != '-'))
+                if (section == 1 && !(argv[++i] && argv[i][0] != '-'))
                 {
                     warnx("Invalid arguments for -c option");
                     errx(1, "Usage: -c <command>");
                 }
                 break;
             case SHOPT:
+                //if (!section)
+                //activates shopt variables
                 break;
             case NORC:
+                //if (!section)
+                //deactivates ressource reader
                 break;
             case AST:
+                //if (!section)
+                //ast print
                 break;
             case VERSION:
+                if (!section)
+                {
+                    printf("Version 0.3\n");
+                    exit(0);
+                }
                 break;
             default:
-                if (section != 1)
+                if (section != 2)
                 {
                     warnx("Unrecognized option");
                     errx(1, "Usage: ./42sh [options] [file]");
                 }
-        }
-    }
-    return 1;
-}
-
-// Do actions according to each options
-void options(char *argv[])
-{
-    if (!check_options(argv))
-        return;
-
-    enum option opt = NONE;
-    size_t section = 0;
-
-    for (size_t i = 1; argv[i]; i++)
-    {
-        section = argv[i][0] == '-' ? 0 : 1;
-        opt = get_option(argv[i], section);
-        switch (opt)
-        {
-            case CMD:
-                printf("-c args OK\n");
-                break;
-            case SHOPT:
-                //activates shopt variables
-                break;
-            case NORC:
-                //deactivates ressource reader
-                break;
-            case AST:
-                //ast print
-                break;
-            case VERSION:
-                printf("Version 0.3\n");
-                exit(0);
-            default:
-                printf("All good\n");
         }
     }
 }
