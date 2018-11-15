d7406d0284133f53c99a7158920bbb50de29f167
louis.holleville <louis.holleville@epita.fr>
2018-11-15 12:39:09 +0100

    [AST] Added functions toa dd elt and pre in ast_node_scmp

--- src/parser/ast/ast_node_scmd.c
+++ src/parser/ast/ast_node_scmd.c
@@ -1,25 +1,38 @@
 #include <stdlib.h>
+#include <err.h>
 #include "parser.h"
 #include "ast.h"
 
-static struct ast_node_scmd *create_ast_node_scmd_intern(char **prefix,
-        char **elements)
+static struct ast_node_scmd *create_ast_node_scmd_intern(void)
 {
     struct ast_node_scmd *new = malloc(sizeof(struct ast_node_scmd));
     if (!new)
         return NULL;
-    new->prefix = prefix;
-    new->elements = elements;
+    new->prefix = malloc(8 * sizeof(char*));
+    if (!new->prefix)
+    {
+        free(new);
+        return NULL;
+    }
+    new->elements = malloc(8 * sizeof(char*));
+    if (!new->elements)
+    {
+        free(new->prefix);
+        free(new);
+    }
+    new->elt_capacity = 8;
+    new->pre_capacity = 8;
+    new->elt_size = 0;
+    new->pre_size = 0;
     return new;
 }
 
-struct ast_node *create_ast_node_scmd(char **prefix, char **elements)
+struct ast_node *create_ast_node_scmd(void)
 {
     struct ast_node *new = malloc(sizeof(struct ast_node));
     if (!new)
         return NULL;
-    struct ast_node_scmd *under_node = create_ast_node_scmd_intern(prefix,
-            elements);
+    struct ast_node_scmd *under_node = create_ast_node_scmd_intern();
     if (!under_node)
     {
         free(new);
@@ -28,4 +41,72 @@ struct ast_node *create_ast_node_scmd(char **prefix, char **elements)
     new->type = N_SCMD;
     new->son->token_SCMD = under_node;
     return new;
+}
+
+void add_prefix_scmd(struct ast_node *node, char *prefix)
+{
+    if (!node || !prefix)
+    {
+        warnx("could not add prefix in simple command node: node or prefix\
+                is empty");
+        return;
+    }
+    if (node->type != N_SCMD)
+    {
+        warnx("could not add prefix in simple command node: node is not\
+                simple command node");
+        return;
+
+    }
+    struct ast_node_scmd *cur = node->son->token_SCMD;
+    if (cur->pre_size == cur->pre_capacity)
+    {
+        char **new = realloc(cur->prefix, 2 * cur->pre_size);
+        if (!new)
+        {
+            warnx("realloc failed in add_prefix_scmd(%s)", prefix);
+            return;
+        }
+        cur->prefix = new;
+        cur->pre_capacity *= 2;
+    }
+    cur->prefix[cur->pre_size] = prefix;
+    cur->pre_size += 1;
+}
+
+void add_element_scmd(struct ast_node *node, char *element)
+{
+    if (!node || !element)
+    {
+        warnx("could not add element in simple command node: node or element\
+                is empty");
+        return;
+    }
+    if (node->type != N_SCMD)
+    {
+        warnx("could not add element in simple command node: node is not\
+                simple command node");
+        return;
+
+    }
+    struct ast_node_scmd *cur = node->son->token_SCMD;
+    if (cur->elt_size == cur->elt_capacity)
+    {
+        char **new = realloc(cur->elements, 2 * cur->elt_size);
+        if (!new)
+        {
+            warnx("realloc failed in add_element_scmd(%s)", element);
+            return;
+        }
+        cur->elements = new;
+        cur->elt_capacity *= 2;
+    }
+    cur->elements[cur->elt_size] = element;
+    cur->elt_size += 1;
+}
+
+void destroy_ast_node_scmd(struct ast_node *node)
+{
+    struct ast_node_scmd *cur = node->son->token_SCMD;
+    free(node->
 }
