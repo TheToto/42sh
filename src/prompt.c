d42d6e868359714d7431b8b1c2d818ed05094878
Sabrina Meng <sabrina.meng@epita.fr>
2018-11-20 17:48:22 +0100

    [HISTORY][ADD] Add history management

--- src/prompt.c
+++ src/prompt.c
@@ -3,6 +3,8 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 #include <err.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 #include "execution.h"
 #include "lexer.h"
@@ -39,12 +41,18 @@ static int exec_prompt(char *str, struct variables *library)
 
 int show_prompt(void)
 {
+    HIST_ENTRY **hist = history_list();;
     struct variables *library = init_var();
     while (1)
     {
         char *buf = readline("[42sh@pc]$ ");
+        if (buf && *buf)
+            add_history(buf);
         exec_prompt(buf, library);
         free(buf);
+        write_history(".42sh_history");
     }
+    for (int i = 0; i < history_length; i++)
+        free_history_entry(hist[i]);
     destroy_var(library);
 }
