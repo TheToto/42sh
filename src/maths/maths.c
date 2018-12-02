5fc8390007b0bb9208aeec8f1968be157b14c9de
Thomas Lupin <thomas.lupin@epita.fr>
2018-12-02 18:13:27 +0100

    [MATHS] Handle -num and +num

--- src/maths/maths.c
+++ src/maths/maths.c
@@ -10,6 +10,11 @@ static int is_digit(char c)
     return c >= '0' && c <= '9';
 }
 
+static int is_num(char c)
+{
+    return (c >= '0' && c <= '9') || c == '+' || c == '-';
+}
+
 static int priority(char op)
 {
     switch (op)
@@ -47,13 +52,22 @@ static int compute_op(int a, int b, char op)
 static int get_number(char *str, size_t *i)
 {
     int val = 0;
-    while(*i < strlen(str) && is_digit(str[*i]))
+    int less = 0;
+    if (str[*i] == '-')
+    {
+        less = 1;
+        (*i)++;
+    }
+    else if (str[*i] == '+')
+        (*i)++;
+    for (; *i < strlen(str) && str[*i] == ' '; (*i)++);
+    while (*i < strlen(str) && is_digit(str[*i]))
     {
         val = (val * 10) + (str[*i] - '0');
         (*i)++;
     }
     (*i)--;
-    return val;
+    return less ? -val : val;
 }
 
 static int destroy_maths(struct stack *values, struct stack *sign)
@@ -72,26 +86,42 @@ static void compute_next(struct stack *values, struct stack *sign)
     push_stack(values, compute_op(left, right, op));
 }
 
+static int math_error(char *str, int want_num,
+        struct stack *values, struct stack *sign)
+{
+    destroy_stack(values);
+    destroy_stack(sign);
+    if (want_num)
+        warnx("libmath : invalid number '%s'", str);
+    else
+        warnx("libmath : invalid operator '%s'", str);
+    return 0;
+}
+
+
 int evaluate_maths(char *str)
 {
     struct stack *values = init_stack();
     struct stack *sign = init_stack();
-
+    int want_num = 1;
     for (size_t i = 0; i < strlen(str); i++)
     {
         if (str[i] == ' ')
             continue;
-        else if (str[i] == '(')
+        else if (str[i] == '(' && want_num)
             push_stack(sign, str[i]);
-        else if (is_digit(str[i]))
-            push_stack(values, get_number(str, &i));
-        else if (str[i] == ')')
+        else if (str[i] == ')' && !want_num)
         {
             while (!is_empty_stack(sign) && peak_stack(sign) != '(')
                 compute_next(values, sign);
             pop_stack(sign);
         }
-        else
+        else if (is_num(str[i]) && want_num)
+        {
+            push_stack(values, get_number(str, &i));
+            want_num = 0;
+        }
+        else if (priority(str[i]) != 0 && !want_num)
         {
             while (!is_empty_stack(sign)
                     && priority(peak_stack(sign)) >= priority(str[i]))
@@ -99,9 +129,19 @@ int evaluate_maths(char *str)
                 compute_next(values, sign);
             }
             push_stack(sign, str[i]);
+            want_num = 1;
+        }
+        else
+        {
+            return math_error(str + i, want_num, values, sign);
         }
     }
     while (!is_empty_stack(sign))
         compute_next(values, sign);
     return destroy_maths(values, sign);
+}
+
+int main(void)
+{
+    return evaluate_maths("- 3 +(3+2)*2");
 }
