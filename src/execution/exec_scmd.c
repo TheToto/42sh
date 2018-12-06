161c3f9aed6d1f1e93c4d427a23ffecedbedc0c3
Thomas Lupin <thomas.lupin@epita.fr>
2018-12-06 13:05:35 +0100

    [EXEC SCMD] Fix write error

--- src/execution/exec_scmd.c
+++ src/execution/exec_scmd.c
@@ -157,7 +157,7 @@ int exec_scmd(struct ast_node_scmd *scmd, struct variables *var)
     for (size_t i = 0; i < scmd->pre_size; i++)
         assign_prefix(var, scmd->prefix[i]);
     char **expanded = replace_var_scmd(scmd);
-    if (scmd->elt_size > 0)
+    if (scmd->elt_size > 0 && expanded[0])
     {
         status = execute(expanded, status, var);
     }
