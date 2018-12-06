5f58a933150bc33d482ebc27889ba89a33e26ed1
Thomas Lupin <thomas.lupin@epita.fr>
2018-12-06 15:14:20 +0100

    [MATHS] Fix priorities

--- src/maths/maths.c
+++ src/maths/maths.c
@@ -30,19 +30,20 @@ static int priority(int op)
 {
     switch (op)
     {
-        case '+':
-        case '-':
-            return 1;
-        case '*':
-        case '/':
-            return 2;
-        case '*' * 2:
         case '&':
         case '&' * 2:
         case '|':
         case '|' * 2:
         case '^':
+            return 1;
+        case '+':
+        case '-':
+            return 2;
+        case '*':
+        case '/':
             return 3;
+        case '*' * 2:
+            return 4;
         default:
             break;
     }
