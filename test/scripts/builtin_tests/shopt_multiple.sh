0dc903d02c6305621bf3fcbb608a48cedb32954d
Sabrina Meng <sabrina.meng@epita.fr>
2018-12-06 18:04:31 +0100

    [TESTSUITE][ADD] Add test of shopt builtin

---
+++ test/scripts/builtin_tests/shopt_multiple.sh
@@ -0,0 +1,7 @@
+shopt -q -u -nope
+shopt -q -u sourcepath -nope
+shopt -s sourcepath -q -nope
+shopt nocaseglob sourcepath -q
+shopt -q -s nocaseglob extglob
+shopt -q sourcepath extglob
+shopt -q -u -s nope
